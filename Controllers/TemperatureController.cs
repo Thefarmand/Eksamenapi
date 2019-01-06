c53b99f33f39e419c21196030dfb231b235dded4
Thefarmand <31881760+Thefarmand@users.noreply.github.com>
2019-01-06 17:53:36 +0100

    Initial commit

---
+++ Controllers/TemperatureController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Library;
+using Library.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eksamen19122018.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemperatureController : ControllerBase
+    {
+        private string connectionstring = "Server=tcp:thefarmand.database.windows.net,1433;Initial Catalog=Thefarmand;Persist Security Info=False;User ID=Thefarmand;Password=Killer1963;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        // GET: api/Temperature
+        [HttpGet(Name = "GetAll")]
+        public IEnumerable<Meassurement> Get()
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query = new SqlCommand("SELECT * FROM meassurement", conn);
+            conn.Open();
+            SqlDataReader laeser = query.ExecuteReader();
+            List<Meassurement> TempList = new List<Meassurement>();
+            if (laeser.HasRows)
+            {
+                while (laeser.Read())
+                {
+                    Meassurement tl = new Meassurement
+                    {      
+                        Id = Convert.ToInt32(laeser[0]),
+                        Pressure = Convert.ToDouble(laeser[1]),
+                        Humidity = Convert.ToDouble(laeser[2]),
+                        Temperature = Convert.ToDouble(laeser[3]),
+                        Timestamp = Convert.ToDateTime(laeser[4])
+                    };
+                    TempList.Add(tl);
+                }
+            }
+            conn.Close();
+            return TempList;
+        }
+
+        // GET: api/Temperature/1
+        [HttpGet("{id}", Name = "GetOne")]
+        public List<Meassurement> GetOne(int id)
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query = new SqlCommand($"SELECT * FROM  meassurement WHERE id={id}", conn);
+            conn.Open();
+            SqlDataReader laeser = query.ExecuteReader();
+            List<Meassurement> tl = new List<Meassurement>();
+
+            if (laeser.HasRows)
+            {
+                while (laeser.Read())
+                {
+                    Meassurement cs = new Meassurement
+                    {
+                        Id = Convert.ToInt32(laeser[0]),
+                        Pressure = Convert.ToDouble(laeser[1]),
+                        Humidity = Convert.ToDouble(laeser[2]),
+                        Temperature = Convert.ToDouble(laeser[3]),
+                        Timestamp = Convert.ToDateTime(laeser[4])
+                    };
+                    tl.Add(cs);
+                }
+            }
+            conn.Close();
+            return tl;
+        }
+
+        // POST: api/Temperature/
+        [HttpPost]
+        public void Post([FromBody] Meassurement value)
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query = new SqlCommand($"INSERT INTO meassurement (pressure, humidity, temperature)" +
+                                              $" VALUES({value.Pressure}, {value.Humidity}, {value.Temperature})", conn);
+            conn.Open();
+            query.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        // PUT: api/Temperature/
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Meassurement value)
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query = new SqlCommand($"UPDATE meassurement SET pressure = {value.Pressure}, humidity = {value.Humidity}, temperature = {value.Temperature} WHERE id={id}", conn);
+            conn.Open();
+            query.ExecuteNonQuery();
+            conn.Close();
+        }
+
+
+
+        // DELETE: api/Temperature/3
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand query = new SqlCommand($"DELETE FROM meassurement WHERE id = {id}", conn);
+            conn.Open();
+            query.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
---
+++ Model/Meassurements.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eksamen19122018.Model
+{
+    public class Meassurements
+    {
+        //public int _id { get; set; }
+        //public double _pressure { get; set; }
+        //public double _humidity { get; set; }
+        //public double _temperature { get; set; }
+        //public DateTime _timestamp { get; set; }
+
+        private int _id;
+        private double _pressure;
+        private double _humidity;
+        private double _temperature;
+        private DateTime _timestamp;
+
+        public Meassurements(int id, double pressure, double humidity, double temperature, DateTime timestamp)
+        {
+            _id = id;
+            _pressure = pressure;
+            _humidity = humidity;
+            _timestamp = timestamp;
+            _temperature = temperature;
+        }
+
+        public Meassurements()
+        {
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public double Pressure
+        {
+            get => _pressure;
+            set => _pressure = value;
+        }
+
+        public double Humidity
+        {
+            get => _humidity;
+            set => _humidity = value;
+        }
+
+        public double Temperature
+        {
+            get => _temperature;
+            set => _temperature = value;
+        }
+
+        public DateTime Timestamp
+        {
+            get => _timestamp;
+            set => _timestamp = value;
+        }
+
+        public override string ToString()
+        {
+            return ($"{nameof(Id)}:{Id}, {nameof(Pressure)}:{Pressure}, {nameof(Humidity)}:{Humidity}, {nameof(Temperature)}:{Temperature}, {nameof(Timestamp)}:{Timestamp}");
+        }
+    }
+}
